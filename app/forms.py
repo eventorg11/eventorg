f893315377732842cd2647929749575394f7597a
eventorg11 <eventorg@kolabnow.com>
2025-12-05 12:28:33 +0400

    Created registration form

---
+++ app/forms.py
@@ -0,0 +1,63 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+class CustomUserCreationForm(UserCreationForm):
+    """Форма регистрации пользователя"""
+
+    email = forms.EmailField(
+        required=True,
+        label="Email",
+        widget=forms.EmailInput(
+            attrs={"class": "form-control", "placeholder": "Введите email"}
+        ),
+    )
+
+    first_name = forms.CharField(
+        required=True,
+        label="Имя",
+        max_length=150,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Введите имя"}
+        ),
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        label="Фамилия",
+        max_length=150,
+        widget=forms.TextInput(
+            attrs={"class": "form-control", "placeholder": "Введите фамилию"}
+        ),
+    )
+
+    class Meta:
+        model = User
+        fields = (
+            "username",
+            "email",
+            "first_name",
+            "last_name",
+            "password1",
+            "password2",
+        )
+        widgets = {
+            "username": forms.TextInput(
+                attrs={
+                    "class": "form-control",
+                    "placeholder": "Введите имя пользователя",
+                }
+            ),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields["password1"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Введите пароль"}
+        )
+        self.fields["password2"].widget.attrs.update(
+            {"class": "form-control", "placeholder": "Подтвердите пароль"}
+        )
