e3160d977e17ac356d04be38942aa92dadcd0c8f
eventorg11 <eventorg@kolabnow.com>
2025-12-05 11:17:31 +0400

    Connected template to backend

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from app import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
-    path('about/', views.about, name='about'),
+    path("", views.index, name="index"),
+    path("about/", views.about, name="about"),
+    path("events/", views.events, name="events"),
 ]
--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
+from app.models import Conference
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 
 def about(request):
-    return render(request, 'about.html')
+    return render(request, "about.html")
+
+
+def events(request):
+    """Страница со списком всех мероприятий"""
+    conferences = Conference.objects.all().order_by("-start_date")
+    return render(request, "events.html", {"conferences": conferences})
